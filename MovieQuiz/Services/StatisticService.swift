ad2f23d78dc1a155383dd836394aa948d6069389
Albert Khaydarov <aghaydarov@gmail.com>
2023-08-25 16:38:29 +0300

    делаю UesrDefaults

--- MovieQuiz/Services/StatisticService.swift
+++ MovieQuiz/Services/StatisticService.swift
@@ -8,12 +8,45 @@
 import Foundation
 
 protocol StatisticService {
+    func store(correct count: Int, total amount: Int)
     var totalAccuracy: Double { get }
     var gamesCount: Int { get }
     var bestGame: GameRecord { get } 
 }
 
 final class StatisticServiceImplementation: StatisticService {
+   
+    private enum Keys: String {
+        case correct, total, bestGame, gamesCount
+    }
+    
+    private let userDefaults = UserDefaults.standard
+        
+    var totalAccuracy: Double
+    
+    var gamesCount: Int
+    
+    var bestGame: GameRecord{
+        get {
+            guard let data = userDefaults.data(forKey: Keys.bestGame.rawValue),
+                let record = try? JSONDecoder().decode(GameRecord.self, from: data) else {
+                return .init(correct: 0, total: 0, date: Date())
+            }
+            return record
+        }        
+        set {
+            guard let data = try? JSONEncoder().encode(newValue) else {
+                print("Невозможно сохранить результат")
+                return
+            }
+            userDefaults.set(data, forKey: Keys.bestGame.rawValue)
+        }
+    }
+    
+    func store(correct count: Int, total amount: Int) {
+        
+    }
+    
 }
 
 
