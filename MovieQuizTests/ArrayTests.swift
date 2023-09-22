51d86d640900103f5eeeccea1726e8c240c0b3e0
aghaydarov@gmail.com <94747384+AlbertKhaydarov@users.noreply.github.com>
2023-09-22 16:22:08 +0300

    Урок по тестированию

--- MovieQuizTests/ArrayTests.swift
+++ MovieQuizTests/ArrayTests.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+import XCTest // не забывайте импортировать фреймворк для тестирования
+@testable import MovieQuiz // импортируем наше приложение для тестирования
+
+class ArrayTests: XCTestCase {
+    func testGetValueInRange() throws { // тест на успешное взятие элемента по индексу
+       // Given
+       let array = ["a", "b", "c", "d"]
+       // When
+       let value = array[safe: 2]
+       // Then
+        XCTAssertNotNil(value)
+        XCTAssertEqual(value, "c")
+    }
+    
+    func testGetValueOutOfRange() throws { // тест на взятие элемента по неправильному индексу
+        let array = [1, 1, 2, 3, 5]
+            
+            // When
+            let value = array[safe: 20]
+            
+            // Then
+            XCTAssertNil(value)
+    }
+}
